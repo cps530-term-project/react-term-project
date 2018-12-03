5a9bd0435f7942e02cf62b3190d025814d54c153
rojina12 <45500987+rojina12@users.noreply.github.com>
2018-12-03 00:42:19 -0600

    Update Credits.js

--- src/Pages/Credits/Credits.js
+++ src/Pages/Credits/Credits.js
@@ -7,7 +7,7 @@ class Credits extends Component {
         <h1>Credits</h1>
         <ul>
         <li>Mohamed Bobat:HTML coding</li>
-        <li>Rojina Dahal: summary about rest and demo and how to...</li>
+        <li>Rojina Dahal: summary about rest and how to...</li>
         <li>Gagan Gill: Initial react setup, deployment and demo page</li>
         <li>Samiran Maheswaran: React summary and conclusion</li>
         </ul>
