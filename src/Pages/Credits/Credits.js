13fb3b2b95c19710f8eed4d2bfb8a7732d983794
Mohamed-Bobat <33939001+Mohamed-Bobat@users.noreply.github.com>
2018-12-03 17:10:16 -0500

    Update Credits.js

--- src/Pages/Credits/Credits.js
+++ src/Pages/Credits/Credits.js
@@ -6,7 +6,7 @@ class Credits extends Component {
       <div >
         <h1>Credits</h1>
         <ul>
-        <li>Mohamed Bobat:HTML coding</li>
+        <li>Mohamed Bobat:HTML coding, Javascript. How to install react.</li>
         <li>Rojina Dahal: summary about rest and how to...</li>
         <li>Gagan Gill: Initial react setup, deployment and demo page</li>
         <li>Samiran Maheswaran: React summary and conclusion</li>
